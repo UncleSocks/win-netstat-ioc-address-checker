ec123e8ad3baf84d66a43ee0f61f4b09826b73a5
UncleSocks <79778613+UncleSocks@users.noreply.github.com>
2024-01-16 09:02:59 +0800

    Update: Modified functions to match PEP8 format.

--- netioc.py
+++ netioc.py
@@ -1,36 +1,36 @@
 import subprocess
 import argparse
 
-nsOutput = subprocess.check_output("netstat -n").decode('ascii').splitlines()
-nsOutputStartLine = nsOutput[5:]
+ns_output = subprocess.check_output("netstat -n").decode('ascii').splitlines()
+ns_output_startline = ns_output[5:]
 
-nsForeignAddress = [output[2] for output in map(str.split, nsOutputStartLine)]
+ns_foreign_address_parser = [output[2] for output in map(str.split, ns_output_startline)]
 
-uniqueForeignAddress = []
-seenForeignAddress = set()
-for entry in nsForeignAddress:
-    foreignAddressAndPort = entry.split(":")
-    foreignAddressOnly = foreignAddressAndPort[0]
+ns_output_startline = []
+seen_foreign_addresses = set()
+for match in ns_foreign_address_parser:
+    foreign_address_and_port = match.split(":")
+    parsed_foreign_address = foreign_address_and_port[0]
 
-    if  foreignAddressOnly not in seenForeignAddress:
-        seenForeignAddress.add(foreignAddressOnly)
-        uniqueForeignAddress.append(foreignAddressOnly)
+    if  parsed_foreign_address not in seen_foreign_addresses:
+        seen_foreign_addresses.add(parsed_foreign_address)
+        ns_output_startline.append(parsed_foreign_address)
 
-parser = argparse.ArgumentParser(description="Netstat IP IOC Checker")
-parser.add_argument("-w","--wordlist", default= "ioc.txt", help="IP address IOC wordlist file location.")
-args = parser.parse_args()
+argument_parser = argparse.ArgumentParser(description="Netstat IP IOC Checker")
+argument_parser.add_argument("-w","--wordlist", default= "ioc.txt", help="IP address IOC wordlist file location.")
+argument = argument_parser.parse_args()
 
 
-iocAddress = []
-with open(args.wordlist) as ioc:
-    for entry in ioc:
+ioc_address_list = []
+with open(argument.wordlist) as ioc_wordlist:
+    for entry in ioc_wordlist:
         entry = entry.strip()
-        iocAddress.append(entry)
-matchedAddress = set(iocAddress).intersection(uniqueForeignAddress)
+        ioc_address_list.append(entry)
+matched_address = set(ioc_address_list).intersection(ns_output_startline)
 
-if not matchedAddress:
+if not matched_address:
     print("No matching blacklisted IP address identified.")
 else:
     print("Machine is currently connected to the following blacklisted IP addresses:")
-    for match in matchedAddress:
+    for match in matched_address:
         print(match)
